617d12963a066323bdbc3040b27349747b2f91c1
Tran Cong Tru <trutc@innotech-vn.com>
2022-05-24 09:27:40 +0700

    1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Tran cong tru')
+  res.send('Tran cong tru 1')
 })
  
 module.exports.handler = serverless(app);
