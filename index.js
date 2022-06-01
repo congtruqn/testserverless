814023f33e22de0bb72ffdf476b05edd7cf1a298
Tran Cong Tru <trutc@innotech-vn.com>
2022-06-01 17:26:37 +0700

    unit

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
- 
+console.log('aaa')
 app.get('/', function (req, res) {
   res.send('Tran cong tru')
 })
 app.get('/trutran', function (req, res) {
-  res.send('Tran cong tru 1')
+  let a =  process.env.APP_DOMAIN
+  res.json({test:a})
 })
 module.exports.handler = serverless(app);
