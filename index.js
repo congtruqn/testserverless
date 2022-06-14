51348dbf10fd124a756cf380313c1a82b3a097f7
Tran Cong Tru <trutc@innotech-vn.com>
2022-06-14 17:19:52 +0700

    1

--- index.js
+++ index.js
@@ -14,13 +14,26 @@ dynamoose.aws.ddb.set(ddb);
 app.get('/', function (req, res) {
   res.send('Tran cong tru')
 })
-app.get('/trutran', function (req, res) {
+app.get('/trutran',async function (req, res) {
   const connection = mysql.createConnection({
     host: 'trutran.cluster-c8ls3dqlbk8b.ap-southeast-1.rds.amazonaws.com',
     port: 3306,
     user: process.env.db_username,
     password: process.env.db_password,
     database: 'trutran'
+  });
+  let sql = `CREATE TABLE Persons (
+    id int,
+    name varchar(255),
+    email varchar(255)
+  );`
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Database created");
+    });
   });
   res.json({test:"a"})
 })
