ffc91044b6b7c43767374849b189eb6d888816f4
Tran Cong Tru <trutc@innotech-vn.com>
2022-06-14 16:52:09 +0700

    1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 const dynamoose = require("dynamoose");
+const mysql = require("mysql");
 console.log(process.env.secretAccessKey)
 const ddb = new dynamoose.aws.sdk.DynamoDB({
   "accessKeyId": process.env.accessKeyId,
@@ -14,12 +15,18 @@ app.get('/', function (req, res) {
   res.send('Tran cong tru')
 })
 app.get('/trutran', function (req, res) {
-  res.json({test:a})
+  const connection = mysql.createConnection({
+    host: process.env.db_username,
+    port: 3306,
+    user: process.env.db_username,
+    password: process.env.db_username,
+    database: process.env.db_username
+  });
 })
 app.get('/test',async function (req, res) {
   const User = dynamoose.model("User", {"id": Number, "name": String});
   try {
-    const user = await User.create({"id": 1, "name": "Tim"}); // If a user with `id=1` already exists in the table, an error will be thrown.
+    const user = await User.create({"id": 1, "name": "Tim"});
     console.log(user);
   } catch (error) {
       console.error(error);
