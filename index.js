4891e27c6734d5451cfff1a7d1c8d387174b303d
Tran Cong Tru <trutc@innotech-vn.com>
2022-06-15 09:49:25 +0700

    1

--- index.js
+++ index.js
@@ -3,40 +3,33 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 const dynamoose = require("dynamoose");
-const mysql = require("mysql");
-console.log(process.env.secretAccessKey)
 const ddb = new dynamoose.aws.sdk.DynamoDB({
   "accessKeyId": process.env.accessKeyId,
   "secretAccessKey": process.env.secretAccessKey,
   "region": "ap-southeast-1"
 });
+const mysql = require('serverless-mysql')({
+  config: {
+    host     : 'trutran.cluster-c8ls3dqlbk8b.ap-southeast-1.rds.amazonaws.com',
+    database : 'trutran',
+    user     : process.env.db_username,
+    password : process.env.db_password
+  }
+})       
 dynamoose.aws.ddb.set(ddb);
 app.get('/', function (req, res) {
   res.send('Tran cong tru')
 })
 app.get('/trutran',async function (req, res) {
   try{
-    const connection = mysql.createConnection({
-      host: 'trutran.cluster-c8ls3dqlbk8b.ap-southeast-1.rds.amazonaws.com',
-      port: 3306,
-      user: process.env.db_username,
-      password: process.env.db_password,
-      database: 'trutran'
-    });
-    let sql = `CREATE TABLE Persons (
-      id int,
-      name varchar(255),
-      email varchar(255)
-    );`
-    connection.connect(function(err) {
-      if (err) throw err;
-      console.log("Connected!");
-      con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Database created");
-      });
-    });
-    res.json({test:"mysql"})
+    let results = await mysql.query('SELECT * FROM Persons')
+ 
+    // Run clean up function
+    await mysql.end()
+   
+    // Return the results
+    res.json(results)
+    //return results
   }
   catch(error){
     console.log(error)
